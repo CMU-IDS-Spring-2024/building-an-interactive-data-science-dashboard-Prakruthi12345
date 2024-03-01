aa03eaad1f259722f41d520d90c70a468fb3c12f
PRAKRUTHI PRADEEP <81363277+Prakruthi12345@users.noreply.github.com>
2024-02-29 23:41:51 -0500

    Update 1_👥_Demographics.py

--- pages/1_👥_Demographics.py
+++ pages/1_👥_Demographics.py
@@ -10,7 +10,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 st.title("Demographics")
 st.markdown("This interactive dashboard supports the exploration of the demographics (age, gender, and race) of the people involved in fatal accidental overdoses in Allegheny County.  You can filter by the year of the overdose incident, as well as the primary drug present in the incident.")
 
-df = pd.read_csv("Downloads/overdose_data_092223.csv")
+df = pd.read_csv("data/overdose_data_092223.csv")
 df.death_date_and_time = pd.to_datetime(df.death_date_and_time)
 
 # to make the visualizations more meaningful, we unabbreviate the race and sex columns
@@ -65,3 +65,4 @@ bar_gender = alt.Chart(df_year_drug, title="Gender").mark_bar().encode(x=alt.X('
 bar_race = alt.Chart(df_year_drug, title="Race").mark_bar().encode(x=alt.X('count()', title="Count of Fatal Overdoses") , y=alt.Y('race', title="")).properties(height=400,width=200)
 
 st.altair_chart(hist_age | bar_gender | bar_race, use_container_width=True)
+
