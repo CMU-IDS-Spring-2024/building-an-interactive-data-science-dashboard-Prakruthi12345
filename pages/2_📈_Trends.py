0cfb8e0b95cafc20fdd9f4e2a27d23f91a016c57
PRAKRUTHI PRADEEP <81363277+Prakruthi12345@users.noreply.github.com>
2024-02-29 23:30:09 -0500

    Update 2_📈_Trends.py

--- pages/2_📈_Trends.py
+++ pages/2_📈_Trends.py
@@ -20,15 +20,15 @@ data = df
 col1, col2 = st.columns([0.1, 0.1],gap="large")
 
 with col1:
-    date_range = st.slider("Range", min_value=data['death_date_and_time'].min().to_pydatetime(), 
+    dates = st.slider("Range", min_value=data['death_date_and_time'].min().to_pydatetime(), 
                        max_value=data['death_date_and_time'].max().to_pydatetime(), value=(data['death_date_and_time'].min().to_pydatetime(), data['death_date_and_time'].max().to_pydatetime()))
-    start_date, end_date = date_range
+   
 
 
 with col2:
-    num_charts = st.number_input("How many top drugs?", min_value=1, max_value=15, value=8)
+    num_output = st.number_input("How many top drugs?", min_value=1, max_value=15, value=8)
 
-
+start_date, end_date = dates
 filtered_data = data[(data['death_date_and_time'] >= start_date) 
 & (data['death_date_and_time'] <= end_date)]
 
@@ -36,18 +36,17 @@ filtered_data = data[(data['death_date_and_time'] >= start_date)
 
 drug_counts = filtered_data.groupby(['case_year', 'combined_od1']).size().reset_index(name='count')
 
-# Find the top prevalent drugs
+# Find top drugs
 
-top_drugs = drug_counts.groupby('combined_od1')['count'].sum().nlargest(num_charts).index
+drug_list = drug_counts.groupby('combined_od1')['count'].sum().nlargest(num_output).index
 
 # Filter the data for the top drugs
-top_drug_data = drug_counts[drug_counts['combined_od1'].isin(top_drugs)]
+top_drugs = drug_counts[drug_counts['combined_od1'].isin(drug_list)]
 
-top_drug_data = top_drug_data.sort_values(by='count', ascending=False)
+sorted_data = top_drugs.sort_values(by='count', ascending=False)
 
-# Create base area chart
 
-base_chart = alt.Chart(top_drug_data).mark_area().encode(
+first = alt.Chart(sorted_data).mark_area().encode(
     x=alt.X('case_year:O', title='Fatal Overdoses per Year'),
     y=alt.Y('count:Q', title=''),
     color=alt.Color('combined_od1:N', title='Primary Drug Involved',legend=None)
@@ -56,14 +55,11 @@ base_chart = alt.Chart(top_drug_data).mark_area().encode(
     height=50
 )
 
-
-# Add row encoding to create multiple area charts
-row_chart = base_chart.encode(
+final = first.encode(
     row=alt.Row(
         'combined_od1:N',
-        title="Primary Drug",sort=top_drugs.tolist() 
+        title="Primary Drug Involved",sort=drug_list.tolist() 
     )
 )
 
-# Display the row chart
-st.altair_chart(row_chart, use_container_width=True)
+st.altair_chart(final, use_container_width=True)
